2c55c23fb085ff552e6a94171817cf7e8514ce7b
Jhonatan Piffer <jhonatan.p.s@gmail.com>
2023-08-09 19:17:59 -0300

    adicionando primeira versão de scripts pra controle da GoPro

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "alias python='python3'" >>  ~/.bashrc
+
+git clone https://github.com/jhonatanpiffer/GoProPy_Bluetooth_Control
+cd GoProPy_Bluetooth_Control/
+
+git clone https://github.com/gopro/OpenGoPro
+
+#Comando para instalar dependencias da lib
+cd GoProPy/OpenGoPro/demos/python/tutorial
+pip install -e .
+cd - 
+
+
